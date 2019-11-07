754b6069ad5933b9ca532e7ce877ef99aa570e26
antonytriana <antonytriana@gmail.com>
2019-11-07 10:41:40 -0500

    Se agrega funcion de transicion y noPerteneceACadena

--- src/model/AFD.java
+++ src/model/AFD.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AFD {
@@ -11,6 +12,7 @@ public class AFD {
     public AFD() {
         this.alfabeto = new Alfabeto();
         this.estados = new Estado();
+        this.transiciones = new ArrayList<Transicion>();
     }
 
     public Estado getEstados() {
@@ -35,6 +37,25 @@ public class AFD {
 
     public void setTransiciones(List<Transicion> transiciones) {
         this.transiciones = transiciones;
+    }
+
+    public boolean noPerteneceACadena(String cadena) {
+        for (int i = 0; i < cadena.length(); i++) {
+            if (!this.alfabeto.getSimbolo().contains(String.valueOf(cadena.charAt(i)))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String funcionTransicion(String estadoActual, String simbolo) {
+        for (Transicion transicion : transiciones) {
+            if (transicion.getEstadoActual().equals(estadoActual)
+                    && transicion.getSimbolo().equals(simbolo)) {
+                return transicion.getEstadoSiguiente();
+            }
+        }
+        return null;
     }
 
 }
